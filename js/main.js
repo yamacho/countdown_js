5af69959ad87a97e35e4bd3766e25fa981b7a049
yamacho <yamacho@users.noreply.github.com>
2017-11-26 17:39:40 +0900

    一時停止、再開ができるようにしよう
    * 一時停止機能の実装
    * 再開機能の実装

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@
   var timeLeft;
   var timeToCountDown = 0;
   var timeId;
+  var isRunning = false;
 
   function updateTimer(t) {
     var d = new Date(t);
@@ -29,6 +30,8 @@
       timeLeft = timeToCountDown - (Date.now() - startTime);
 
       if (timeLeft < 0) {
+        isRunning = false;
+        start.textContent = 'Start';
         clearTimeout(timeId);
         timeLeft = 0;
         timeToCountDown = 0;
@@ -42,8 +45,17 @@
   }
 
   start.addEventListener('click', function() {
-    startTime = Date.now();
-    countDown();
+    if (isRunning === false) {
+      isRunning = true;
+      start.textContent = 'Stop';
+      startTime = Date.now();
+      countDown();
+    } else {
+      isRunning = false;
+      start.textContent = 'Start';
+      timeToCountDown = timeLeft;
+      clearTimeout(timeId);
+    }
   });
 
   min.addEventListener('click', function() {
