120e277dfe9703b8652c90f0429e2361184208d5
yamacho <yamacho@users.noreply.github.com>
2017-11-27 18:03:10 +0900

    タイマーを仕上げていこう
    * カウントダウン中の動作調整
    * 残り時間が 60 分以上になったときの調整

--- js/main.js
+++ js/main.js
@@ -59,12 +59,28 @@
   });
 
   min.addEventListener('click', function() {
+    if (isRunning === true) {
+      return;
+    }
+
     timeToCountDown += 60 * 1000;
+    if (timeToCountDown >= 60 * 60 * 1000) {
+      timeToCountDown = 0;
+    }
+
     updateTimer(timeToCountDown);
   });
 
   sec.addEventListener('click', function() {
+    if (isRunning === true) {
+      return;
+    }
+
     timeToCountDown += 1000;
+    if (timeToCountDown >= 60 * 60 * 1000) {
+      timeToCountDown = 0;
+    }
+
     updateTimer(timeToCountDown);
   });
 
